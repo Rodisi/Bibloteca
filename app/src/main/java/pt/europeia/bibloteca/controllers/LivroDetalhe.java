4a5881b9158777a59334ce0a4f49f139f1b49736
Rodisi <brunomarques87@gmail.com>
2016-10-30 19:15:04 +0000

    livroDetalhe more or less concluded (problem with dates)

--- app/src/main/java/pt/europeia/bibloteca/controllers/LivroDetalhe.java
+++ app/src/main/java/pt/europeia/bibloteca/controllers/LivroDetalhe.java
@@ -1,15 +1,72 @@
 package pt.europeia.bibloteca.controllers;
 
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import pt.europeia.bibloteca.R;
+import pt.europeia.bibloteca.models.Livro;
 
 public class LivroDetalhe extends AppCompatActivity {
+
+    public TextView txttitulo;
+    public TextView txtautor;
+    public TextView txteditora;
+    public TextView txtisbn;
+    public TextView txtdata;
+    public ImageView imgcapa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_livro_detalhe);
+        Livro livro = (Livro) getIntent().getSerializableExtra("livro");
+        txttitulo=(TextView) findViewById(R.id.txttitulo);
+        txtautor=(TextView) findViewById(R.id.txtautor);
+        txteditora=(TextView) findViewById(R.id.txteditora);
+        txtisbn=(TextView) findViewById(R.id.txtisbn);
+        txtdata=(TextView) findViewById(R.id.txtdata);
+        imgcapa=(ImageView) findViewById(R.id.capa);
+
+        txttitulo.setText(livro.getTitulo());
+        txtautor.setText("Autor: "+ livro.getAutor());
+        txteditora.setText("Editora : "+ livro.getEditora());
+        txtisbn.setText("ISBN: "+ livro.getIsbn());
+        long millis = livro.getData() * 1000;
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String  date = sdf.format(millis);
+
+        txtdata.setText("Data; " + date);
+        Bitmap bp = getBitmapFromAsset(livro.getCapa());
+
+        imgcapa.setImageBitmap(bp);
+
+
+
+
+    }
+
+    private Bitmap getBitmapFromAsset(String strName)
+    {
+        AssetManager assetManager = getAssets();
+        InputStream istr = null;
+        try {
+            istr = assetManager.open(strName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Bitmap bitmap = BitmapFactory.decodeStream(istr);
+        return bitmap;
     }
 }
